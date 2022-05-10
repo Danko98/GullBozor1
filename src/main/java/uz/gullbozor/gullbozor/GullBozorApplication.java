d12dff5d789380a7b2f9653b2f8b13a37eb1c127
Davronjon <goldcoder998@gmail.com>
2022-05-10 15:12:12 +0500

    Swagger ulandi

--- src/main/java/uz/gullbozor/gullbozor/GullBozorApplication.java
+++ src/main/java/uz/gullbozor/gullbozor/GullBozorApplication.java
@@ -3,9 +3,11 @@ package uz.gullbozor.gullbozor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class GullBozorApplication {
 
     public static void main(String[] args) {
